8f12eabe6b548da496796715a6af215435601bc9
SHIAK AFRID <afridayan01@gmail.com>
2025-07-23 12:31:19 +0530

    Create OCR_WITH_MISTARL.py

---
+++ OCR_WITH_MISTARL.py
@@ -0,0 +1,102 @@
+# https://docs.mistral.ai/capabilities/OCR/basic_ocr/
+
+
+
+
+
+#With  pdf
+import os
+from mistralai import Mistral
+
+api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=api_key)
+
+ocr_response = client.ocr.process(
+    model="mistral-ocr-latest",
+    document={
+        "type": "document_url",
+        "document_url": "https://arxiv.org/pdf/2201.04234"
+    },
+    include_image_base64=True
+)
+
+
+
+
+#Or passing a Base64 encoded pdf:
+import base64
+import os
+from mistralai import Mistral
+def encode_pdf(pdf_path):
+    """Encode the pdf to base64."""
+    try:
+        with open(pdf_path, "rb") as pdf_file:
+            return base64.b64encode(pdf_file.read()).decode('utf-8')
+    except FileNotFoundError:
+        print(f"Error: The file {pdf_path} was not found.")
+        return None
+    except Exception as e: 
+        print(f"Error: {e}")
+        return None
+pdf_path = "path_to_your_pdf.pdf"
+base64_pdf = encode_pdf(pdf_path)
+api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=api_key)
+ocr_response = client.ocr.process(
+    model="mistral-ocr-latest",
+    document={
+        "type": "document_url",
+        "document_url": f"data:application/pdf;base64,{base64_pdf}" 
+    },
+    include_image_base64=True
+)
+
+
+
+#With image
+import os
+from mistralai import Mistral
+
+api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=api_key)
+
+ocr_response = client.ocr.process(
+    model="mistral-ocr-latest",
+    document={
+        "type": "image_url",
+        "image_url": "https://raw.githubusercontent.com/mistralai/cookbook/refs/heads/main/mistral/ocr/receipt.png"
+    },
+    include_image_base64=True
+)
+
+
+
+
+#Or passing a Base64 encoded image:
+
+import base64
+import os
+from mistralai import Mistral
+def encode_image(image_path):
+    """Encode the image to base64."""
+    try:
+        with open(image_path, "rb") as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
+    except FileNotFoundError:
+        print(f"Error: The file {image_path} was not found.")
+        return None
+    except Exception as e: 
+        print(f"Error: {e}")
+        return None
+image_path = "path_to_your_image.jpg"
+base64_image = encode_image(image_path)
+api_key = os.environ["MISTRAL_API_KEY"]
+client = Mistral(api_key=api_key)
+ocr_response = client.ocr.process(
+    model="mistral-ocr-latest",
+    document={
+        "type": "image_url",
+        "image_url": f"data:image/jpeg;base64,{base64_image}" 
+    },
+    include_image_base64=True
+)
